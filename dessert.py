21bf1812931072ce0f4c7b25f966ad8d6f6879ee
suri-mejias <suri.mejias@ucas-edu.net>
2024-11-26 11:05:03 -0700

    fixed merge conflicts

--- dessert.py
+++ dessert.py
@@ -1,15 +1,10 @@
 from abc import ABC, abstractmethod
-<<<<<<< HEAD
-=======
-
->>>>>>> 913efd367ed4ed04ea9190cb818c1cbb05c028a3
 class Dessert(ABC):
     def __init__(self, name, quantity, price_per_unit):
         self.name = name
         self.quantity = quantity
         self.price_per_unit = price_per_unit
 
-<<<<<<< HEAD
     def calculate_tax(self):
         return self.calculate_cost() * 0.07
     
@@ -17,14 +12,6 @@ class Dessert(ABC):
     def calculate_cost(self):
         return self.quantity * self.price_per_unit
  
-=======
-    @abstractmethod
-    def calculate_cost(self):
-        pass
-
-    def calculate_tax(self):
-        return self.calculate_cost() * 0.07
->>>>>>> 913efd367ed4ed04ea9190cb818c1cbb05c028a3
 
 class Candy(Dessert):
     def __init__(self, name, quantity, price_per_unit):
@@ -54,10 +41,6 @@ class Sundae(IceCream):
         self.topping_price = topping_price
 
     def calculate_cost(self):
-<<<<<<< HEAD
         return super().calculate_cost() + self.topping_price 
     
 
-=======
-        return super().calculate_cost() + self.topping_price
->>>>>>> 913efd367ed4ed04ea9190cb818c1cbb05c028a3
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-udge", 1.29))
-    order.add(Cookie("Oatmeal Raisin", 2, 3.45
