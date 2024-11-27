b10d50c3c70d3ac7d54b6b76674b2c9885567abb
suri-mejias <suri.mejias@ucas-edu.net>
2024-11-27 09:47:52 -0700

    k

--- dessert.py
+++ dessert.py
@@ -1,22 +1,18 @@
 from abc import ABC, abstractmethod
+
 class Dessert(ABC):
     def __init__(self, name, quantity, price_per_unit, tax_percent=7.0):
         self.name = name
         self.quantity = quantity
         self.price_per_unit = price_per_unit
-        self.tax_percent = tax_percent 
+        self.tax_percent = tax_percent
 
-    def calculate_tax(self):
-<<<<<<< HEAD
-        return self.calculate_cost() * (self.tax_percent / 100)
-=======
-        return self.calculate_cost() * 0.07
-    
     @abstractmethod
     def calculate_cost(self):
-        return self.quantity * self.price_per_unit
- 
->>>>>>> 8a017092600d4dc8d0b09e5133e48ef030535055
+        pass
+
+    def calculate_tax(self):
+        return self.calculate_cost() * (self.tax_percent / 100)
 
 class Candy(Dessert):
     def __init__(self, name, quantity, price_per_unit, tax_percent=7.0):
@@ -46,6 +42,4 @@ class Sundae(IceCream):
         self.topping_price = topping_price
 
     def calculate_cost(self):
-        return super().calculate_cost() + self.topping_price 
-    
-
+        return super().calculate_cost() + self.topping_price
