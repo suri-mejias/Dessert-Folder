0813d2ace349ea8edd1495fdf10bbd247bdbc8f6
suri-mejias <suri.mejias@ucas-edu.net>
2024-11-27 09:43:55 -0700

    added the taxt_percent attribute to dessert

--- dessert.py
+++ dessert.py
@@ -1,42 +1,43 @@
 from abc import ABC, abstractmethod
 
 class Dessert(ABC):
-    def __init__(self, name, quantity, price_per_unit):
+    def __init__(self, name, quantity, price_per_unit, tax_percent=7.0):
         self.name = name
         self.quantity = quantity
         self.price_per_unit = price_per_unit
+        self.tax_percent = tax_percent 
 
     @abstractmethod
     def calculate_cost(self):
         pass
 
     def calculate_tax(self):
-        return self.calculate_cost() * 0.07
+        return self.calculate_cost() * (self.tax_percent / 100)
 
 class Candy(Dessert):
-    def __init__(self, name, quantity, price_per_unit):
-        super().__init__(name, quantity, price_per_unit)
+    def __init__(self, name, quantity, price_per_unit, tax_percent=7.0):
+        super().__init__(name, quantity, price_per_unit, tax_percent)
 
     def calculate_cost(self):
         return self.quantity * self.price_per_unit
 
 class Cookie(Dessert):
-    def __init__(self, name, quantity, price_per_unit):
-        super().__init__(name, quantity, price_per_unit)
+    def __init__(self, name, quantity, price_per_unit, tax_percent=7.0):
+        super().__init__(name, quantity, price_per_unit, tax_percent)
 
     def calculate_cost(self):
-        return (self.quantity / 12) * self.price_per_unit  # Assume price per dozen
+        return (self.quantity / 12) * self.price_per_unit
 
 class IceCream(Dessert):
-    def __init__(self, name, quantity, price_per_unit):
-        super().__init__(name, quantity, price_per_unit)
+    def __init__(self, name, quantity, price_per_unit, tax_percent=7.0):
+        super().__init__(name, quantity, price_per_unit, tax_percent)
 
     def calculate_cost(self):
         return self.quantity * self.price_per_unit
 
 class Sundae(IceCream):
-    def __init__(self, name, quantity, price_per_unit, topping_name, topping_price):
-        super().__init__(name, quantity, price_per_unit)
+    def __init__(self, name, quantity, price_per_unit, topping_name, topping_price, tax_percent=7.0):
+        super().__init__(name, quantity, price_per_unit, tax_percent)
         self.topping_name = topping_name
         self.topping_price = topping_price
 
